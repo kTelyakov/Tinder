3ad4553c31a44aa73bc20d34108f30ebdb6c99eb
Eduard <edvard88@inbox.ru>
2019-05-06 15:54:00 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import fb_auth_token
 
-fb_username = "forprojects@inbox.ru"
-fb_password = "dusty95dre@mPass"
+fb_username = "login@mail.ru"
+fb_password = "password"
 
 fb_access_token = fb_auth_token.get_fb_access_token(fb_username, fb_password)
 fb_user_id = fb_auth_token.get_fb_id(fb_access_token)
