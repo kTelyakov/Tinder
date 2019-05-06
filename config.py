939ce07728f26d93ed642480c3fb23d3e5f44bfb
Eduard <edvard88@inbox.ru>
2019-05-06 15:53:07 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+import fb_auth_token
+
+fb_username = "forprojects@inbox.ru"
+fb_password = "dusty95dre@mPass"
+
+fb_access_token = fb_auth_token.get_fb_access_token(fb_username, fb_password)
+fb_user_id = fb_auth_token.get_fb_id(fb_access_token)
+
+#host = 'https://api.gotinder.com'
+#tinder_token = "Your tinder token goes here"
+
+
+#leave tinder_token empty if you don't use phone verification
+
+# Your real config file should simply be named "config.py"
+# Just insert your fb_username and fb_password in string format
+# and the fb_auth_token.py module will do the rest!
